d0b7a43cf0ad320614ef4a09cf25dc57b86f5b52
Mona Javed Syed <monajaved93@gmail.com>
2020-10-23 02:36:29 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -80,8 +80,7 @@ def winner(board)
       return "X"
     elsif (position_1 == "O"  && position_2 == "O" && position_3 == "O")
       return "O"
-    else
-      return nil
     end
+    return nil
   end
 end
